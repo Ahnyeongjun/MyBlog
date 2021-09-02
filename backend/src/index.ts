d186a179a7b7639058d22178393d26cdcb4bf4dd
AhnyoungJun <toinin@naver.com>
2021-09-02 16:57:11 +0900

    [ADD] backend - https server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,21 +8,29 @@ import http from 'http';
 import './config/env.ts';
 import api from './api';
 import db from './db';
-
+import fs from 'fs';
+import path from 'path';
+import https from 'https';
 const app = new Koa();
 const router = new Router();
 
 router.use('', api.routes());
-const options = {
-    origin: '*',
+const cors_options = {
+    origin: 'http://localhost:8080',
+    credentials: true,
 };
-app.use(helmet()).use(cors(options)).use(bodyparser()).use(logger()).use(router.routes()).use(router.allowedMethods());
+app.use(helmet()).use(cors(cors_options)).use(bodyparser()).use(logger()).use(router.routes()).use(router.allowedMethods());
 
 const serverCallback = app.callback();
 const httpServer = http.createServer(serverCallback);
 // 현대모터스
+const options = {
+    key: fs.readFileSync('./private.pem'),
+    cert: fs.readFileSync('./public.pem'),
+};
+const httpsServer = https.createServer(options, serverCallback);
 
 db.then(
-    () => httpServer.listen(process.env.SERVER_PORT || 5000, () => {})
+    () => httpsServer.listen(process.env.SERVER_PORT || 5000, () => {})
     // eslint-disable-next-line no-console
 ).catch(console.error);
