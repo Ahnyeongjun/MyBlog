04ae608299ba261fd0376b387c423f3565581526
AhnyoungJun <toinin@naver.com>
2021-09-02 16:56:56 +0900

    [ADD] backend - postrepository - updateView&&getOneView

--- backend/src/db/repository/views.ts
+++ backend/src/db/repository/views.ts
@@ -1,5 +1,7 @@
 import { EntityRepository, getRepository, Repository } from 'typeorm';
+import { View } from 'typeorm/schema-builder/view/View';
 import connection from '..';
+import { updateViews } from '../../interface/request/views';
 import { Views } from '../entity';
 
 @EntityRepository(Views)
@@ -9,6 +11,30 @@ export class ViewsRepository {
             const views = new Views();
             views.viewsCount = 0;
             await (await connection).manager.save(views);
+
+            return views;
+        } catch (e) {
+            console.log(e);
+        }
+    }
+    public async updataView(req: updateViews) {
+        try {
+            const viewsRepository = (await connection).manager.getRepository(Views);
+
+            const views = await this.getOneView(req.uid);
+            if (views) {
+                return viewsRepository.save({ lock: { mode: 'PESSIMISTIC_READ' }, ...views, ...req });
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    public async getOneView(uid: string) {
+        try {
+            const viewsRepository = (await connection).manager.getRepository(Views);
+
+            const views = await viewsRepository.findOne({ uid: uid });
 
             return views;
         } catch (e) {
