726aea86138ca01369122cbbea0af1b7c417bb26
AhnyoungJun <toinin@naver.com>
2021-09-03 13:23:36 +0900

    [ADD] frontend - routing - tagcount

--- frontend/src/Routing.tsx
+++ frontend/src/Routing.tsx
@@ -4,6 +4,7 @@ import LoginPage from './page/auth/LoginPage';
 import MainPage from './page/main/MainPage';
 import NotFoundPage from './page/notFound/NotFoundPage';
 import PostPage from './page/post/PostPage';
+import TagSearchPage from './page/tagSearch/tagSearchPage';
 import WritePage from './page/wrtie/WritePage';
 const Routing = () => {
     return (
@@ -11,6 +12,7 @@ const Routing = () => {
             <Route exact path="/" component={MainPage} />
             <Route exact path="/login" component={LoginPage} />
             <Route exact path="/write" component={WritePage} />
+            <Route exact path="/tag/:tagName" component={TagSearchPage} />
             <Route path="/post/:searchUrl" component={PostPage} />
             <Route component={NotFoundPage} />
         </Switch>
