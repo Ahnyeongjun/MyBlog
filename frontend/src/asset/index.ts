1f88707468c005bb5fa07ff0197730e0763ea4fe
AhnyoungJun <toinin@naver.com>
2021-10-29 10:04:05 +0900

    [FIX] frontend - image webf

--- frontend/src/asset/index.ts
+++ frontend/src/asset/index.ts
@@ -1,7 +1,7 @@
 export { default as Favicon } from './favicon/favicon.ico';
 export { default as Spring } from './spring.svg';
-export { default as Garabageimage2 } from './garbageImage2.jpg';
-export { default as Coffee } from './garbageImage3.jpg';
-export { default as JavaScript } from './javascript.png';
+export { default as Garabageimage2 } from './garbageImage2.webp';
+export { default as Coffee } from './garbageImage3.webp';
+export { default as JavaScript } from './javascript.webp';
 export { default as MyImage } from './myImage.svg';
 export { default as DefaultImage } from './favpng_information.svg';
